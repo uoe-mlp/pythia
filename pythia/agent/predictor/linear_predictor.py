a801298f83b3b45d04887c8599315793ed445aba
GrowlingM1ke <46566684+GrowlingM1ke@users.noreply.github.com>
2021-02-06 18:51:33 +0100

    Feature/linear predictor (#15)
    * Started work on window size
    
    * Implemented window size + unit tests for lin_pred
    
    * Fixed minor error with unit tests
    
    Co-authored-by: GrowlingM1ke <dragontooth95@gmail.com>

--- pythia/agent/predictor/linear_predictor.py
+++ pythia/agent/predictor/linear_predictor.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from typing import Dict, Tuple
 from abc import ABC, abstractclassmethod
-from torch import Tensor
+from torch import Tensor, empty, flatten
 from torch.optim import Adam, Optimizer
 from torch.nn import MSELoss, Module
 
@@ -13,7 +13,7 @@ from .predictor import Predictor
 
 class LinearPredictor(Predictor):
 
-    def __init__(self, input_size: int, output_size: int, learning_rate: float, weight_decay: float, epochs: int, predict_returns: bool):
+    def __init__(self, input_size: int, output_size: int, window_size: int, learning_rate: float, weight_decay: float, epochs: int, predict_returns: bool):
         super(LinearPredictor, self).__init__(input_size, output_size, predict_returns)
         self.learning_rate: float = learning_rate
         self.weight_decay: float = weight_decay
@@ -21,6 +21,7 @@ class LinearPredictor(Predictor):
         self.optimizer: Optimizer = Adam(self.model.parameters(), lr=learning_rate, weight_decay=weight_decay)
         self.loss: Module = MSELoss()
         self.epochs: int = epochs
+        self.window_size: int = window_size
 
     @staticmethod
     def initialise(input_size: int, output_size: int, params: Dict) -> Predictor:
@@ -28,20 +29,46 @@ class LinearPredictor(Predictor):
         learning_rate: float = ArgsParser.get_or_default(params, 'learning_rate', 1e-3)
         epochs: int = ArgsParser.get_or_default(params, 'epochs', 100)
         predict_returns: bool = ArgsParser.get_or_default(params, 'predict_returns', False)
-        return LinearPredictor(input_size=input_size, output_size=output_size, learning_rate=learning_rate, weight_decay=weight_decay, epochs=epochs, predict_returns=predict_returns)
+        window_size: int = ArgsParser.get_or_default(params, 'window_size', 1)
+        return LinearPredictor(input_size=input_size, output_size=output_size, window_size=window_size, learning_rate=learning_rate, weight_decay=weight_decay, epochs=epochs, predict_returns=predict_returns)
 
     def fit(self, X: Tensor, Y: Tensor, **kwargs):
-        lag: int = ArgsParser.get_or_default(kwargs, 'lag', 1)
+        """
+        Description:
+            The X and Y tensors are data representative of the same day.
+            Since the aim is to predict next day price, we need to lag
+            the Y Tensor by an index (a day).
+        """
         if self.predict_returns:
-            X = X[1:,:]
+            X = X[:-1,:]
             Y = Y[1:,:] / Y[:-1,:] - 1
-
-        for epoch in range(self.epochs):
-            self.optimizer.zero_grad()
-            outputs = self.model(X[:-lag,:])
-            loss = self.loss(outputs, Y[lag:,:])
-            loss.backward()
-            self.optimizer.step()
+        else:
+            X = X[:-1,:]
+            Y = Y[1:,:]
+        if self.window_size == 1:
+            for epoch in range(self.epochs):
+                self.optimizer.zero_grad()
+                outputs = self.model(X)
+                loss = self.loss(outputs, Y)
+                loss.backward()
+                self.optimizer.step()
+        
+        else:
+            X, Y = self.__reshape_window(X, Y)
+            for epoch in range(self.epochs):
+                self.optimizer.zero_grad()
+                outputs = self.model(X[:-1,:])
+                loss = self.loss(outputs, Y[1:,:])
+                loss.backward()
+                self.optimizer.step()
+        
+    def __reshape_window(self, X: Tensor, Y: Tensor):
+        Y = Y[self.window_size-1:,:]
+        X_new = empty(len(X)-self.window_size+1, self.window_size * len(X[0]))
+        for i in range(len(X)-self.window_size+1):
+            # In the case of linear predict, flatten
+            X_new[i] = flatten(X[i:self.window_size+i])
+        return X_new, Y
 
     def predict(self, X: Tensor) -> Tuple[Tensor, Tensor]:
         """
