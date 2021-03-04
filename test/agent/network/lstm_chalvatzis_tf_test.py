034b2b883a50fd4e5fb5f904fbfffa45ff21e969
Alessandro Facchin <alessandro.facchin@outlook.com>
2021-03-04 23:03:17 +0100

    Fixed test

--- test/agent/network/lstm_chalvatzis_tf_test.py
+++ test/agent/network/lstm_chalvatzis_tf_test.py
@@ -43,7 +43,7 @@ def test_lstm_chalvatzis_tf_smoke():
     net = LSTMChalvatzisTF(input_size=1, window_size=window_size, hidden_size=[16, 16], output_size=1, dropout=[0,0])
     net.compile(optimizer='adam', loss='mse', metrics=['mae'])
 
-    net.fit(X_train, Y_train, epochs=5, validation_data=(X_val, Y_val), callbacks=[OutputObserver(net, X_train, Y_hat=Y_train)])
+    net.fit(X_train, Y_train, epochs=5, validation_data=(X_val, Y_val), callbacks=[OutputObserver(net, X_train, Y_hat=Y_train, epochs=5)])
 
     net.summary()
 
