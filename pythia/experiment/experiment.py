8e7a96f6712274f3b471b3539fc93978d365973a
alessandrofacchin <39587443+alessandrofacchin@users.noreply.github.com>
2021-03-06 18:39:37 +0100

    Feature/benchmark (#25)
    * Edited default trader to cheap version
    
    * Including benchmark
    
    * Adding benchmark on tests
    
    * Benchmark in experiments
    
    * BuynHold from implicit to explicit
    
    * Changed default agent and predictor in supervised agent
    
    * Handling of edge cases
    
    * New test settings
    
    * New param example

--- pythia/experiment/experiment.py
+++ pythia/experiment/experiment.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from typing import Dict, Optional
 from abc import ABC, abstractmethod, abstractstaticmethod
+from copy import deepcopy
 
 from pythia.journal import Journal
 from pythia.market import Market
@@ -9,17 +10,19 @@ from pythia.agent import Agent
 
 class Experiment(ABC):
 
-    def __init__(self, path: str, market: Market, agent: Agent, journal: Journal, settings: Dict):
+    def __init__(self, path: str, market: Market, agent: Agent, journal: Journal, benchmark: Optional[Agent], settings: Dict):
         self.path: str = path
         self.market: Market = market
         self.agent: Agent = agent
         self.journal: Journal = journal
+        self.benchmark: Optional[Agent] = benchmark
+        self.benchmark_journal: Optional[Journal] = deepcopy(journal) if benchmark else None
         self.settings: Dict = settings
         # TODO: add self.seed
 
     @staticmethod
     @abstractstaticmethod
-    def initialise(path: str, market: Market, agent: Agent, journal: Journal, settings: Dict, params: Dict=None) -> Experiment: pass
+    def initialise(path: str, market: Market, agent: Agent, journal: Journal, benchmark: Optional[Agent], settings: Dict, params: Dict=None) -> Experiment: pass
 
     @abstractmethod
     def run(self): pass
